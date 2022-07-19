490acc9dea428a28ee957cefefff4ebf29a7b060
kawsar ahmed <webdeveloper.kawsar99@gmail.com>
2022-07-19 11:11:48 +0600

    added some data

--- main.js
+++ main.js
@@ -3,18 +3,30 @@ const tbody=document.getElementById('tbody')
 fetch('https://donor-server.herokuapp.com/donors')
   .then(response => response.json())
   .then(data =>{
-     for(one of data ){
+     for(one of data.reverse() ){
+
+// try date 
+
+const newDate=new Date(one.date)  //donation date  
+const date_2=new Date() //today 
+let difference =newDate.getTime() -date_2.getTime() 
+
+let totalDays=Math.ceil(difference/(1000*3600*24))
+
+
         const tr=document.createElement('tr')
         tr.innerHTML=`
        
-        <td data-label="Name">${one.name} </td>
-        <td data-label="Blood Group">${one.blood}</td>
-        <td data-label="Address">${one.address} </td>
-        <td data-label="Phone">${one.phone}</td>
-        <td data-label="date">${one.date}</td>
-        <td data-label="Passed">12 days</td>
-        <td>edit</td>
-        <td>DELETE</td>
+        <td data-label="নাম">${one.name} </td>
+        <td data-label="রক্তের গ্রুপ">${one.blood}</td>
+        <td data-label="ঠিকানা">${one.address} </td>
+        <td data-label="মোবাইল">${one.phone}</td>
+        <td data-label="সর্বশেষ রক্তদানের তারিখ">${one.date}</td>
+        <td data-label="অতিবাহিত হয়েছে">${Math.abs(totalDays)}</td>
+        <div id="action"> 
+        <td><img onclick='editBtnClick()'  src='images/edit.svg' width='20' height='20'/> <img src='images/delete.svg' onclick="deleteBtnClick('${one._id}')" width='20' height='20'/></td>
+        
+        <div/>
         
         `  
         
@@ -46,4 +58,89 @@ function myFunction() {
         }
       }
     }
+  }
+
+
+  // open modal 
+  let modalBtn = document.getElementById("modal-btn")
+  let modal = document.querySelector(".modal")
+  let closeBtn = document.querySelector(".close-btn")
+  modalBtn.onclick = function() {
+    modal.style.display = "block"
+  }
+  closeBtn.onclick = function() {
+    modal.style.display = "none"
+  }
+  window.onclick = function(e) {
+    if (e.target == modal) {
+      modal.style.display = "none"
+    }
+  }
+
+  function submitForm() {
+    const name = document.getElementById('name').value
+    const address = document.getElementById('address').value
+
+    const phone = document.getElementById('phone').value
+    
+    //select value
+    
+ var select = document.getElementById('group');
+var blood = select.options[select.selectedIndex].value;
+//console.log(value); // en
+const date=document.getElementById('donation-date').value
+    
+    
+    const donor = {
+      name, 
+      blood,
+      date,
+      address, 
+      phone }
+    
+    fetch('https://donor-server.herokuapp.com/donors', {
+        method: 'POST',
+        body: JSON.stringify(donor),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      .then((response) => response.json())
+      .then((json) =>
+      {
+        console.log(json)
+        location.reload()
+    
+      });
+    console.log(donor)
+    //   location.reload()
+    //  modal.style.display='none'
+  } 
+
+
+  // edit button clicl
+
+
+  function editBtnClick(){
+    alert("edit button click")
+  }
+
+  function deleteBtnClick(item){ 
+    const proceed=window.confirm('are you sure ?') 
+    if(proceed){
+      const url=`https://donor-server.herokuapp.com/donors/${item}` 
+      fetch(url,{
+        method:'DELETE'
+      }) 
+      .then(res=>res.json())
+      .then(date=>{
+        // console.log(data); 
+        //  alert("deleted") 
+      window.location.reload()
+
+       
+      })  
+
+    }
+    // alert(item)
   }
